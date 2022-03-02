e48b2784d7c5dd6b26e1d13b8bc684a68fc4594b
LaphatsanunPuangpansak <97969278+LaphatsanunPuangpansak@users.noreply.github.com>
2022-03-02 13:58:15 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -16,6 +16,12 @@
         <td><img src="https://files.gqthailand.com/uploads/d_20180522111636.jpg" width="45px"></td>
         <td><a href="https://web.facebook.com/AdamLevine?_rdc=1&_rdr">Facebook</a></td>
       </tr>
+      <tr>
+        <td>1</td>
+        <td>Jason Statham</td>
+        <td><img src="https://www.looper.com/img/gallery/jason-statham-shares-action-packed-behind-the-scenes-look-at-expendables-4/l-intro-1635901499.jpg" width="45px"></td>
+        <td><a href="https://web.facebook.com/JasonStatham?_rdc=1&_rdr">Facebook</a></td>
+      </tr>
     </table>
   </body>
 </html>
