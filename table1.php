a927fcd5eb5ab6f6967fc12a7e74b3af51acc978
LaphatsanunPuangpansak <97969278+LaphatsanunPuangpansak@users.noreply.github.com>
2022-03-02 13:43:42 +0700

    Update table1.php

--- table1.php
+++ table1.php
@@ -5,14 +5,16 @@
   <body>
     <table border="1">
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
-        <td>คอลัมน์ที่ 3</td>
+        <th>ลำดับ</th>
+        <th>ชื่อ</th>
+        <th>รูปภาพ</th>
+        <th>Twitter/FB</th>
       </tr>
       <tr>
-        <td>คอลัมน์ที่ 1</td>
-        <td>คอลัมน์ที่ 2</td>
-        <td>คอลัมน์ที่ 3</td>
+        <td>1</td>
+        <td>Adam Levine</td>
+        <td><img src="https://files.gqthailand.com/uploads/d_20180522111636.jpg" width="45px"></td>
+        <td><a href="https://web.facebook.com/AdamLevine?_rdc=1&_rdr">Facebook</a></td>
       </tr>
     </table>
   </body>
